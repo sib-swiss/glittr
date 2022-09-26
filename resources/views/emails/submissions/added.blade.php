50c41d82b85fef9390c7d551b9ecc189938be6b9
Yann Haefliger <Yann.Haefliger@sib.swiss>
2022-09-26 17:05:46 +0200

    submission mail content

--- resources/views/emails/submissions/added.blade.php
+++ resources/views/emails/submissions/added.blade.php
@@ -1,9 +1,23 @@
 @component('mail::message')
 # A new repository has been submitted!
+
+@component('mail::table')
+| Field          | Value                  |
+| -------------: | ---------------------- |
+| **Url**        | [{{ $submission->url }}]({{ $submission->url }}) |
+| **Tags**       | {{ $submission->tags->implode('name', ', ') }} |
+| **Submitter**  | {{ $submission->name }} ({{ $submission->email }}) |
+@endcomponent
+
+@if ($submission->comment != '')
+## Comment
+@component('mail::panel')
+{{ $submission->comment }}
+@endcomponent
+@endif
+
 @component('mail::button', ['url' => route('admin.dashboard')])
 Go to the dashboard
 @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
 @endcomponent
