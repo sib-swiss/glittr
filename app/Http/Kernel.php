090e800a1146e6b7f4daf8d94721b290329ea2c6
Yann Haefliger <Yann.Haefliger@sib.swiss>
2026-03-02 16:24:07 +0100

    Early hints preload response header

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,7 @@ class Kernel extends HttpKernel
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'early.hints' => \App\Http\Middleware\AddEarlyHintsHeaders::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \App\Http\Middleware\ValidateSignature::class,
